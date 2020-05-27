656b6278f653605fb4218d4893f1345112c6050f
Dorel <dorel.paul@gmail.com>
2020-05-27 10:42:09 +0200

    Adding constants

---
+++ PBAppliModules/constants.py
@@ -0,0 +1,13 @@
+pbhost = "Pur Beurre Host"
+pbuser = "Pur Beurre User"
+pbpassword = "User Password"
+pbdatabase = "purbeurredb"
+
+categories = ["Boissons", "Viandes", "Produits laitiers", "Plats préparés", 
+		"Produits à tartiner", "Biscuits et gâteaux", "Charcuteries", 
+		"Epicerie", "Petit-déjeuners", "Fromages", "Desserts", "Sauces", 
+		"Produits de la mer", "Conserves", "Surgelés"]
+nutrition_grades = ["A","B","D","E"]
+urlp1 = "https://fr.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tag_0="
+urlp2 = "&tagtype_1=nutrition_grades&tag_contains_1=contains&tag_1="
+urlp3 = "&page_size=20&json=true"
