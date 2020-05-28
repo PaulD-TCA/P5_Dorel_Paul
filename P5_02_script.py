032f368b4a07ce3e9dc457ef61cb0ddd2770c3d8
Dorel <dorel.paul@gmail.com>
2020-05-28 16:47:25 +0200

    Final version of the script

--- P5_02_script.py
+++ P5_02_script.py
@@ -7,14 +7,19 @@ import requests
 import mysql.connector
 
 #modules import
-from PBAppliModules import database_initialization
+from PBAppliModules import database_initialization, database_filling, constants
 
 if __name__ == '__main__':
 	#Instances creation
-	database = database_initialization.database_ini()#
+	database = database_initialization.database_ini(constants)#
+	filling = database_filling.database_fill(constants)
 
 	#Data base initialisation
 	database.db_ini()# purbeurre data base initialisation
 	database.table_products()# Product table initialisation
 	database.table_categories()# Categories table initialisation
 	database.table_backup()# Backup table initialisation
+
+	#Database filling
+	filling.built_data_products()# Filling Products table with Open Food Facts database
+	filling.build_data_categories()# Filling Categories table with Open Food Facts database
