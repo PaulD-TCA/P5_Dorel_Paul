ab57a5f094e12dc46f87df40dcfce30bdeb1e09a
Dorel <dorel.paul@gmail.com>
2020-06-03 21:14:46 +0200

    first try to split queries from python main file

--- P5_02_script.py
+++ P5_02_script.py
@@ -8,6 +8,7 @@ import mysql.connector
 
 #modules import
 from PBAppliModules import database_initialization, database_filling, constants
+from PBAppliModules import MYSQL_QUERIES
 
 if __name__ == '__main__':
 	#Instances creation
@@ -16,10 +17,17 @@ if __name__ == '__main__':
 
 	#Data base initialisation
 	database.db_ini()# purbeurre data base initialisation
-	database.table_products()# Product table initialisation
-	database.table_categories()# Categories table initialisation
-	database.table_backup()# Backup table initialisation
+#	database.table_products()# Product table initialisation
+#	database.table_categories()# Categories table initialisation
+#	database.table_backup()# Backup table initialisation
+	database.tables_initialization(MYSQL_QUERIES)
+
+	#database.brouillon(MYSQL_QUERIES)
 
 	#Database filling
-	filling.built_data_products()# Filling Products table with Open Food Facts database
-	filling.build_data_categories()# Filling Categories table with Open Food Facts database
+	filling.built_data_products(MYSQL_QUERIES)# Filling Products table with Open Food Facts database
+#	filling.build_data_categories()# Filling Categories table with Open Food Facts database
+
+	filling.brouillonfilling(MYSQL_QUERIES)
+
+	print("Data base is done")
