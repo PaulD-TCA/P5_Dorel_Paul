b81d9a76872557dd245a5a30a376934e17d836e0
Dorel <dorel.paul@gmail.com>
2020-06-21 17:08:33 +0200

    scrip final version

--- P5_02_script.py
+++ P5_02_script.py
@@ -7,27 +7,36 @@ import requests
 import mysql.connector
 
 #modules import
-from PBAppliModules import database_initialization, database_filling, constants
-from PBAppliModules import MYSQL_QUERIES
+from PBAppliModules import database_initialization
+from PBAppliModules import database_filling
+from PBAppliModules import mysql_queries
+from PBAppliModules import constants
+from PBAppliModules import user_data
+
+
+def main():
+    """
+    Main file who generate the data base "Pur Beurre".
+      1. Data base initialization.
+         - a data base named "purbeurredb" is initialized.
+         - 3 tables are initialized.
+      2. Data base filling.
+         - Table "Products".
+         - Table "Categories".
+    """
+    #Instances creation
+    database = database_initialization.DatabaseIni()
+    filling = database_filling.DatabaseFill()
+
+    #Data base initialisation
+    database.db_ini(user_data, mysql_queries)
+    database.tables_initialization(user_data, mysql_queries)
+
+    #Database filling
+    filling.built_data_products(constants, user_data, mysql_queries)
+    filling.build_data_categories(constants, user_data, mysql_queries)
+    print("Data base is done")
+
 
 if __name__ == '__main__':
-	#Instances creation
-	database = database_initialization.database_ini(constants)#
-	filling = database_filling.database_fill(constants)
-
-	#Data base initialisation
-	database.db_ini()# purbeurre data base initialisation
-#	database.table_products()# Product table initialisation
-#	database.table_categories()# Categories table initialisation
-#	database.table_backup()# Backup table initialisation
-	database.tables_initialization(MYSQL_QUERIES)
-
-	#database.brouillon(MYSQL_QUERIES)
-
-	#Database filling
-	filling.built_data_products(MYSQL_QUERIES)# Filling Products table with Open Food Facts database
-#	filling.build_data_categories()# Filling Categories table with Open Food Facts database
-
-	filling.brouillonfilling(MYSQL_QUERIES)
-
-	print("Data base is done")
+	main()
