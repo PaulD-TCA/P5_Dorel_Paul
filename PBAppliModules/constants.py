a7998d6df75f85bd2bb5de8eb573519473287d1b
Dorel <dorel.paul@gmail.com>
2020-06-21 17:15:44 +0200

    constants last version

--- PBAppliModules/constants.py
+++ PBAppliModules/constants.py
@@ -1,21 +1,25 @@
-DB_NAME = 'purbeurredb'
+#! /usr/bin/env python3
+# coding: utf-8
+
+CATEGORIES = ["Boissons", "Viandes", "Produits laitiers", "Plats préparés",
+		"Produits à tartiner", "Biscuits et gâteaux", "Charcuteries",
+		"Epicerie", "Petit-déjeuners", "Fromages", "Desserts", "Sauces",
+		"Produits de la mer", "Conserves", "Surgelés"]
+NUTRITION_GRADES = ["A","B","D","E"]
+URL_PART_1 = "https://fr.openfoodfacts.org/cgi/search.pl?action="\
+            "process&tagtype_0=categories&tag_contains_0=contains&tag_0="
+URL_PART_2 = "&tagtype_1=nutrition_grades&tag_contains_1=contains&tag_1="
+URL_PART_3 = "&page_size=500&json=true"
+#20 or 50 or 100 or 250 or 500 or 1000
 
 
-CATEGORIES = ["Boissons", "Viandes"]#, "Produits laitiers", "Plats préparés"]#,
-		#"Produits à tartiner", "Biscuits et gâteaux", "Charcuteries",
-		#"Epicerie", "Petit-déjeuners", "Fromages", "Desserts", "Sauces",
-		#"Produits de la mer", "Conserves", "Surgelés"]
-nutrition_grades = ["A","B"]#,"D","E"]
-url_part_1 = "https://fr.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tag_0="
-url_part_2 = "&tagtype_1=nutrition_grades&tag_contains_1=contains&tag_1="
-url_part_3 = "&page_size=50&json=true"
-
-#20 ou 50 ou 100 ou 250 ou 500 ou 1000
-
-
-url_page_open_food_facts = "https://fr.openfoodfacts.org/produit/"
+URL_OPEN_FOOD_FACTS = "https://fr.openfoodfacts.org/produit/"
 
 PB_HEADER_DISPLAY = (
+				"\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n"
+				"\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n"
+				"\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n""\n"
+
 				"############################################################\n"
 				"#                                                          #\n"
 				"#               PUR BEURRE ALIEMENTS SAINS                 #\n"
@@ -30,7 +34,6 @@ PB_FOOTER_DISPLAY = (
 				"#           puis validez avec la touche \"entrée\"           #\n"
 				"#                                                          #\n"
 				"############################################################\n"
-				"\n""\n""\n""\n""\n""\n""\n""\n"
 				)
 
 PB_WELCOME_DISPLAY = (
@@ -49,4 +52,33 @@ PB_WELCOME_DISPLAY = (
 				"    2 Consulter les résultats d'une précédente recherche    \n"
 				"                                                            \n"
 				"    3 Quitter                                               \n"
+				)
+
+PB_SUBSTITUTION_DISPLAY = (
+				"                                                            \n"
+				"                  ________________________                  \n"
+				"                                                            \n"
+				"               Que désirez vous faire?                      \n"
+				"                                                            \n"
+				"                                                            \n"
+				"    1 Sauvegarder votre recherche                           \n"
+				"                                                            \n"
+				"    2 Effectuer une nouvelle recherche                      \n"
+				"                                                            \n"
+				"    3 Quitter                                               \n"
+				)
+
+PB_CATEGORIES_DISPLAY = (
+
+				"                  Catégories proposées:                     \n"
+				)
+
+PB_PROPOSITION_JF_DISPLAY = (
+
+				"           Proposition d'aliments à substituer:             \n"
+				)
+
+PB_PROPOSITION_HF_DISPLAY = (
+
+				"           Proposition d'aliments à substituer:             \n"
 				)
