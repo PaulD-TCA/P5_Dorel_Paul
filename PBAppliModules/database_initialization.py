a7fdce2ecae7eabc5b90f1420f221c4b4d4a08fb
Dorel <dorel.paul@gmail.com>
2020-06-21 17:13:18 +0200

    DB initialization last version

--- PBAppliModules/database_initialization.py
+++ PBAppliModules/database_initialization.py
@@ -3,59 +3,60 @@
 
 import requests
 import mysql.connector
+from mysql.connector import errorcode
 
-class database_ini:
-	def __init__(self,constants):
-		self.host = constants.pbhost
-		self.user = constants.pbuser
-		self.password = constants.pbpassword
-		self.database = constants.pbdatabase
-		
 
-	def db_ini(self):# purbeurre data base initialisation
-		mysql_login = mysql.connector.connect(user = self.user, 
-			password = self.password, host = self.host)
-		cursor = mysql_login.cursor()
-		cursor.execute("DROP DATABASE IF EXISTS purbeurredb")
-		cursor.execute("CREATE DATABASE purbeurredb")
-		mysql_login.close()
+class DatabaseIni:
 
-	def table_products(self):# Product table initialisation
-		database_login = mysql.connector.connect(user = self.user, 
-			password = self.password, host = self.host, database = self.database)
-		cursor = database_login.cursor()
-		cursor.execute("""CREATE TABLE Products (
-			p_id int unsigned NOT NULL AUTO_INCREMENT,
-			p_code varchar(50), 
-			p_name varchar(200),
-			p_nutrition_grade_fr text(1),
-			p_ingredients_text varchar(2000),
-			p_store varchar(200),
-			p_compared_to_category varchar(200),
-			p_url varchar(1000),
-			categories_id tinyint(3),
-			PRIMARY KEY (p_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;""")
-		database_login.close()
+    """
+    Database initialization
+      1. Initialize Pur Beurre database.
+      2. Initialize tables.
+    """
 
-	def table_categories(self):# Categories table initialisation
-		database_login = mysql.connector.connect(user = self.user, 
-			password = self.password, host = self.host, database = self.database)
-		cursor = database_login.cursor()
-		cursor.execute("""CREATE TABLE Categories (
-			c_id int unsigned NOT NULL AUTO_INCREMENT,
-			c_id2 int,
-			c_name varchar(100),
-			PRIMARY KEY (c_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;""")
-		database_login.close()
+    def db_ini(self, user_data, mysql_queries):
+        """
+        Database initialisation
+        @return:
+          1. Delete a former database.
+          2. Create a new Mysql database.
+        @type user_data: dictionary
+        @param user_data: contain user datas for connextion to the database
+                        (host, user, password and database name)
+        @type mysql_queries: string
+        @param mysql_queries: SQL queries used with mysql connector.
+        """
+        mysql_login = mysql.connector.connect(**user_data.DATA)
+        cursor = mysql_login.cursor()
+        cursor.execute(mysql_queries.DROP_DB)
+        cursor.execute(mysql_queries.CREATE_DB)
+        cursor.close()
+        mysql_login.close()
 
-	def table_backup(self):# Backup table initialisation
-		database_login = mysql.connector.connect(user = self.user, 
-			password = self.password, host = self.host, database = self.database)
-		cursor = database_login.cursor()
-		cursor.execute("""CREATE TABLE Backup (
-			b_id int unsigned NOT NULL AUTO_INCREMENT,
-			b_date date,
-			products_id int,
-			PRIMARY KEY (b_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;""")
-		database_login.close()
-
+    def tables_initialization(self, user_data, mysql_queries):
+        """
+        Tables initialization
+        @type user_data: dictionary
+        @param user_data: contain user datas for connextion to the database
+        @type mysql_queries: string
+        @param mysql_queries: SQL queries used with mysql connector.
+        @return: Create tables with instructions from MySQL instructions.
+          stored in a file "mysql_queries" If tables are here already, the
+          sript will print "already exist". Else the scrip will print "OK"
+        """
+        cnx = mysql.connector.connect(**user_data.DATA)
+        cursor = cnx.cursor()
+        for table_name in mysql_queries.TABLES:
+            table_description = mysql_queries.TABLES[table_name]
+            try:
+                print("Creating table {}: ".format(table_name), end='')
+                cursor.execute(table_description)
+            except mysql.connector.Error as err:
+                if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                    print("already exists.")
+                else:
+                    print(err.msg)
+            else:
+                print("OK")
+        cursor.close()
+        cnx.close()
