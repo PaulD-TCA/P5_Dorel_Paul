2e962b3e53acb18ee816538a8ffce706a05d4536
Dorel <dorel.paul@gmail.com>
2020-06-21 17:10:47 +0200

    filling last version

--- PBAppliModules/database_filling.py
+++ PBAppliModules/database_filling.py
@@ -5,77 +5,91 @@ import json
 import requests
 import mysql.connector
 
-class database_fill:
+class DatabaseFill:
 
-	def __init__(self,constants):
-		self.host = constants.pbhost
-		self.user = constants.pbuser
-		self.password = constants.pbpassword
-		self.database = constants.pbdatabase
-		self.categories = constants.categories
-		self.nutrition_grades = constants.nutrition_grades
-		self.urlp1 = constants.urlp1
-		self.urlp2 = constants.urlp2
-		self.urlp3 = constants.urlp3
-		self.urlpageoff = constants.urlpageoff
-		self.resp = ""
-		self.p_id = 0
-		self.p_code = ""
-		self.p_name = ""
-		self.p_nutrition_grade_fr = ""
-		self.p_ingredients_text = ""
-		self.p_store = ""
-		self.p_categories_hierarchy = ""
-		self.p_compared_to_category = ""
-		self.p_url = ""
-		self.categories_id = ""
-		self.c_id = ""
-		self.c_name = ""
+    """
+    Fill "Pur Beurre" database with data taken from
+    Open Food Facts API and data from "constants" file.
+    """
 
-	def built_data_products(self):# purbeurre data base initialisation
-	
-		for categorie in self.categories:
-			self.c_id = self.categories.index(categorie)
-			for nutrition_grade in self.nutrition_grades:
-				resp = requests.get(self.urlp1+categorie+
-					self.urlp2+nutrition_grade+self.urlp3)
-				data = resp.json()
-				nb_prods = int(data["count"])# le nombre de produits de la recherche
-				page_s = int(data["page_size"])
-				prodtowritte = 0
-				if page_s <= nb_prods:
-					maxproduct = page_s
-				else: 
-					maxproduct = nb_prods
-				while prodtowritte < maxproduct:
-					try:			
-						database_login = mysql.connector.connect(user = self.user, 
-							password = self.password, host = self.host, database = self.database)
-						cursor = database_login.cursor()						
-						self.p_id = self.p_id + 1
-						self.p_code = data["products"][prodtowritte]["code"]
-						self.p_name = data["products"][prodtowritte]["product_name"]
-						self.p_nutrition_grade_fr = data["products"][prodtowritte]["nutrition_grade_fr"]
-						self.p_ingredients_text = data["products"][prodtowritte]["ingredients_text"]
-						self.p_store = data["products"][prodtowritte]["stores"]
-						self.p_compared_to_category = data["products"][prodtowritte]["compared_to_category"]
-						self.p_url = self.urlpageoff + self.p_code
-						self.categories_id = self.c_id + 1
-						prodtowritte = prodtowritte +1
-						cursor.execute("INSERT INTO Products (p_code, p_name, p_nutrition_grade_fr, p_ingredients_text, p_store, p_compared_to_category, p_url, categories_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (self.p_code, self.p_name, self.p_nutrition_grade_fr, self.p_ingredients_text, self.p_store, self.p_compared_to_category, self.p_url, self.categories_id))
-						database_login.commit()
-						database_login.close()
-					except:
-						prodtowritte = prodtowritte +1
+    def built_data_products(self,constants, user_data, mysql_queries):
+        """
+        Fill the table "Products" with datas from Open Food Facts API.
+        @type constant: string
+        @param constant: Parts of the text to show.
+        @type user_data: dictionary
+        @param user_data: contain user datas for connextion to the database
+                        (host, user, password and database name)
+        @type mysql_queries: string
+        @param mysql_queries: SQL queries used with mysql connector.
+        @return:
+          1. Get a JSON file from API.
+          2. Read the JSON file.
+          3. Product by product we get the wanted data. If data are missing we
+          go to the next product.
+          4. Store data in the table.
+        """
+        cnx = mysql.connector.connect(**user_data.DATA)
+        cursor = cnx.cursor()
+        for categorie in constants.CATEGORIES:
+            categories_id = constants.CATEGORIES.index(categorie)+1
+            for nutrition_grade in constants.NUTRITION_GRADES:
+                resp = requests.get(constants.URL_PART_1+categorie+
+                    constants.URL_PART_2+nutrition_grade+constants.URL_PART_3)
+                data = resp.json()
+                nb_prods = int(data["count"])
+                page_s = int(data["page_size"])
+                row_w = 0
+                p_id = 1
+                if page_s <= nb_prods:
+                    maxproduct = page_s
+                else:
+                    maxproduct = nb_prods
+                while row_w < maxproduct:
+                    try:
+                        p_id = p_id + 1
+                        p_code = data["products"][row_w]["code"]
+                        p_name = data["products"][row_w]["product_name"]
+                        p_name = p_name.replace('"', "'")
+                        p_nut_gr = data["products"][row_w]["nutrition_grade_fr"]
+                        p_ingre_t = data["products"][row_w]["ingredients_text"]
+                        p_store = data["products"][row_w]["stores"]
+                        p_ca_tags = data["products"][row_w]["categories_tags"]
+                        p_ca_tags = str.join(",",p_ca_tags)
+                        p_ca_h = data["products"][row_w]["categories_tags"]
+                        p_ca_h = str.join(",",p_ca_h)
+                        p_url = constants.URL_OPEN_FOOD_FACTS + p_code
+                        row_w = row_w + 1
+                        data_prod =(p_code, p_name, p_nut_gr, p_ingre_t,
+                                    p_store, p_ca_tags, p_url, categories_id)
+                        cursor.execute(mysql_queries.DATA_PRODUCTS, data_prod)
+                        cnx.commit()
+                    except:
+                        row_w = row_w + 1
+            print("Categorie "+categorie+" ok. ("+str(categories_id)+ "/15)")
 
-	def build_data_categories(self):
-
-		for categorie in self.categories:
-			database_login = mysql.connector.connect(user = self.user, 
-				password = self.password, host = self.host, database = self.database)
-			cursor = database_login.cursor()
-			c_idtofill = 1
-			cursor.execute("INSERT INTO Categories (c_id2, c_name) VALUES (%s, %s)",(c_idtofill, categorie))
-			c_idtofill = c_idtofill + 1
-			database_login.commit()
-			database_login.close()
+    def build_data_categories(self,constants, user_data, mysql_queries):
+        """
+        @type constant: string
+        @param constant: Parts of the text to show.
+        @type user_data: dictionary
+        @param user_data: contain user datas for connextion to the database
+                        (host, user, password and database name)
+        @type mysql_queries: string
+        @param mysql_queries: SQL queries used with mysql connector.
+        @return: Fill the table Categories with a categorie list taken from the file
+        "constants".
+        """
+        cnx = mysql.connector.connect(**user_data.DATA)
+        cursor = cnx.cursor()
+        c_idtofill = 1
+        for categorie in constants.CATEGORIES:
+            data_categorie = {
+            'c_idtofill': c_idtofill,
+            'categorie': categorie
+            }
+            cursor.execute(mysql_queries.DATA_CATEGORIES, data_categorie)
+            c_idtofill = c_idtofill + 1
+            cnx.commit()
+        cursor.close()
+        cnx.close()
