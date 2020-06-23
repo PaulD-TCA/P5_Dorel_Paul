0d631d6505100c748388804f8419fd3ecd39c67a
Dorel <dorel.paul@gmail.com>
2020-06-23 07:25:03 +0200

    last commit main program

--- P5_03_Code_Source_Main.py
+++ P5_03_Code_Source_Main.py
@@ -1,20 +1,32 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-import mysql.connector
 
-#modules import
-from PBAppliModules import PBeurre_Screen1, PBeurre_Screen2, PBeurre_Screen3, constants
-from PBAppliModules import MYSQL_QUERIES
-
-if __name__ == '__main__':
-	#Instances creation
-	sc1 = PBeurre_Screen1.Screen1(constants)
-	sc2 = PBeurre_Screen2.Screen2(constants)
-	sc3 = PBeurre_Screen3.Screen3(constants)
+from PBAppliModules import pb_main_loop
+from PBAppliModules import user_choice
+from PBAppliModules import pbeurre_screen1
+from PBAppliModules import pbeurre_screen2
+from PBAppliModules import pbeurre_screen3
+#from PBAppliModules import constants
 
 
-#	sc1.welcome()
-#	sc1.categories_screen()
-#	sc2.lookforproducts(MYSQL_QUERIES)
-	sc3.substitution(MYSQL_QUERIES)
+
+def main():
+	"""
+	Main file. Make instances and activate the main loop.
+	  - sc1 : display the welcome screen and the backup screen
+	  - sc2 : display the categories screen and results of the junk food query
+	  - sc3 : display a substitution product and save it
+	"""
+	#Instance creation
+	sc1 = pbeurre_screen1.Screen1()
+	sc2 = pbeurre_screen2.Screen2()
+	uc = user_choice.UserChoice()
+	appli_initialisation = pb_main_loop.AppliManagement()
+	sc3 = pbeurre_screen3.Screen3()
+
+	#Main loop activation
+	appli_initialisation.appli_main_loop(sc1, sc2, sc3, uc)
+
+if __name__ == "__main__":
+	main()
